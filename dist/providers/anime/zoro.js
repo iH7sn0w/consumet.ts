446e53256495325842489cd725bb64e9ee0e5164
n0 sn0w <11691884+iH7sn0w@users.noreply.github.com>
2023-10-31 03:35:42 +0500

    Update zoro.js

--- dist/providers/anime/zoro.js
+++ dist/providers/anime/zoro.js
@@ -216,16 +216,12 @@ class Zoro extends models_1.AnimeParser {
                 throw err;
             }
         };
-        this.retrieveServerId = ($, index, subOrDub) => {
-  const rtr_sv_id = $(`div.ps_-block.ps_-block-sub.servers-${subOrDub} > div.ps__-list > div`)
-    .map((i, el) => {
-      const $el = $(el);
-      return $el.attr('data-server-id') === `${index}` ? $el.attr('data-id') : null;
-    })
-    .get()[0];
-
-  return rtr_sv_id;
-};
+      this.retrieveServerId = ($, index, subOrDub) => {
+            return $(`div.ps_-block.ps_-block-sub.servers-${subOrDub} > div.ps__-list > div`)
+                .map((i, el) => ($(el).attr('data-server-id') == `${index}` ? $(el) : null))
+                .get()[0]
+                .attr('data-id');
+        };
 
         /**
          * @param page Page number
